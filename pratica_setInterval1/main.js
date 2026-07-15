5464d5066f1061caa9a64782423ba2bb041358b4
pier-ex <nathanaelrog@hotmail.com>
2026-07-14 22:34:49 -0300

    study: introdução setInterval e setTimeout

---
+++ pratica_setInterval1/main.js
@@ -0,0 +1,73 @@
+
+// console.log('carregando')
+
+// setTimeout(function() {
+//     console.log('pronto')
+// }, 2000)
+
+
+// let numero = 10
+
+// const contador = setInterval(function() {
+//     console.log(numero)
+
+//     numero--
+
+//     if(numero < 0) {
+//         clearInterval(contador)
+//     }
+// },1000)
+
+// const botao = document.querySelector('.Enviar')
+
+// botao.addEventListener('click', function() {
+//     console.log('enviar')
+//     setTimeout(function() {
+//         console.log('mensagem removida')
+//     }, 5000)
+// })
+
+
+// console.log('iniciando..')
+
+// let timer = setTimeout(function() {
+//     console.log('finalizado')
+// },4000)
+
+// let fila = setTimeout(function() {
+//     console.log('você demorou')
+// }, 5000)
+
+// clearTimeout(fila)
+
+// let rodagem = setInterval(function(){
+//     console.log('estou rodando..')
+// })
+
+// clearInterval(rodagem)
+
+// let contagem = 0
+
+// let contador = setInterval(function(){
+//     if (contagem < 6) {
+//         contagem++
+//         clearInterval(contagem)
+//     }
+//     console.log(contagem)
+// }, 1000)
+
+
+// let segundaContagem = 10
+
+// let segundoContador = setInterval(function() {
+//     console.log(segundaContagem)
+//     if (segundaContagem > 0) {
+//         segundaContagem--
+//         console.log(segundaContagem)
+//     }
+//     clearInterval(segundaContagem)
+// }, 1000)
+
+
+
+
