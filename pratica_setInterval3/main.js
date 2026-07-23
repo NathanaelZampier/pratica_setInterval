380d1962ad7a368e1c1d2756a8c4fcac780057d7
pier-ex <nathanaelrog@hotmail.com>
2026-07-23 15:21:44 -0300

    study: callbacks com objetos, parametros e setTimeout

--- pratica_setInterval3/main.js
+++ pratica_setInterval3/main.js
@@ -232,23 +232,23 @@
 // buscarCidade(retornoCidade)
 
 
-function verificarNumero(numero, callback) {
-    if (numero % 2 === 0) {
-        callback(true)
-    } else {
-        callback(false)
-    }
-}
+// function verificarNumero(numero, callback) {
+//     if (numero % 2 === 0) {
+//         callback(true)
+//     } else {
+//         callback(false)
+//     }
+// }
 
-function retornoNumero(par) {
-    if (par) {
-        console.log('numero par')
-    } else {
-        console.log('numero impar')
-    }   
-}
+// function retornoNumero(par) {
+//     if (par) {
+//         console.log('numero par')
+//     } else {
+//         console.log('numero impar')
+//     }   
+// }
 
-verificarNumero(4, retornoNumero)
+// verificarNumero(4, retornoNumero)
 
 // function buscarProduto(callback) {
 //     setTimeout(function(){
@@ -268,3 +268,197 @@ verificarNumero(4, retornoNumero)
 
 // buscarProduto(retornoProduto)
 
+
+// function calcularMedia(n1,n2,n3, callback) {
+//     let total = n1 + n2 + n3
+//     let media = total / 3
+//     callback(media)
+// }
+
+// function retornoMedia(media) {
+//     console.log(`media: ${media}`)
+// }
+
+// calcularMedia(7,6,9, retornoMedia)
+
+
+// function buscarLivro(callback) {
+//     setTimeout(function(){
+//         callback({
+//             titulo: 'clean code',
+//             autor: 'robert C. martin',
+//             paginas: 464
+//         })
+//     },2000)
+// }
+
+// function retornoLivro(dados) {
+//     console.log(dados.titulo)
+//     console.log(dados.autor)
+//     console.log(dados.paginas)
+// }
+
+// buscarLivro(retornoLivro)
+
+
+// function verificarIdade(idade, callback) {
+//     if (idade >= 18) {
+//         callback(true)
+//     } else {
+//         callback(false)
+//     }
+// }
+
+// function retornoIdade(verdade) {
+//     if (verdade) {
+//         console.log('maior idade')
+//     } else {
+//         console.log('menor idade')
+//     }
+// }
+
+// verificarIdade(18, retornoIdade)
+
+
+// function fazerLogin(usuario, senha, callback) {
+//     console.log('verificando login...')
+
+//     if (usuario === 'admin' && senha === 1234) {
+//         callback(true)
+//     } else {
+//         callback(false)
+//     }
+// }
+
+// function retornoLogin(verdade) {
+//     if (verdade) {
+//         console.log('login realizado')
+//     } else {
+//         console.log('Usuário ou senha invalidos')
+//     }
+// }
+
+// fazerLogin('admin', 1234, retornoLogin)
+
+
+// function buscarCarro(callback) {
+//     setTimeout(function(){
+//         callback({
+//             marca: 'toyota',
+//             modelo: 'corolla',
+//             ano: 2022,
+//             preco: 135000
+//         })
+//     },2000)
+// }
+
+
+// function retornoCarro(dados) {
+//     console.log(dados.marca + ' ' + dados.modelo) 
+//     console.log(dados.ano)
+//     console.log(dados.preco)
+// }
+
+// buscarCarro(retornoCarro)
+
+
+// function multiplicar(a,b, callback) {
+//     let multi = a * b
+//     callback(multi)
+// }
+
+// function retornoMulti(multi) {
+//     console.log(`resultado ${multi}`)
+// }
+
+// function quadradoMulti(multi) {
+//     let quadrado = multi * multi
+//     console.log(`O quadrado do resultado é: ${quadrado}`)
+// }
+
+// multiplicar(10,20, retornoMulti)
+
+// function calcularIMC(peso, altura, callback) {
+//     let calculo = peso / (altura * altura)
+//     callback(calculo)
+// }
+
+// function retornoIMC(calculo) {
+//     console.log(`IMC: ${calculo}`)
+// }
+
+// calcularIMC(80, 1.86, retornoIMC)
+
+
+// function buscarAluno(callback) {
+//     setTimeout(function(){
+//         callback({
+//             nome: 'marcos',
+//             curso: 'engenharia',
+//             periodo: 5
+//         })
+//     },2000)
+// }
+
+
+// function retornoAluno(dados) {
+//     console.log(dados.nome)
+//     console.log(dados.curso)
+//     console.log(dados.periodo)
+// }
+
+// buscarAluno(retornoAluno)
+
+
+// function verificarSenha(senha, callback) {
+//     if (senha.length >= 6) {
+//         callback(true)
+//     } else {
+//         callback(false)
+//     }
+// }
+
+
+// function retornoSenha(ehForte) {
+//     if (ehForte) {
+//         console.log('senha valida')
+//     } else {
+//         console.log('senha invalida')
+//     }
+// }
+
+// verificarSenha(31231231, retornoSenha)
+
+// function buscarPedido(callback) {
+//     setTimeout(function(){
+//         callback({
+//             numero: 1024,
+//             cliente: 'pedro',
+//             total: 189.90
+//         })
+//     },2000)
+// }
+
+// function retornoPedido(dados) {
+//     console.log(dados.numero)
+//     console.log(dados.cliente)
+//     console.log(dados.total)
+// }
+
+// buscarPedido(retornoPedido)
+
+function calcular(a,b, callback) {
+    let soma = a + b
+    let multiplicacao = a * b
+    callback({
+        somar: soma,
+        multiplicaçao: multiplicacao,
+    })
+}
+
+function retornoCalcular(dados) {
+    console.log(dados.somar)
+    console.log(dados.multiplicaçao)
+}
+
+calcular(10, 20, retornoCalcular)
